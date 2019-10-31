16b98ff1bc6ed13168b0c6b3862aa268cfbd14c8
PAULO VICTOR MATHIAS SILVA <pvmathiassilva@gmail.com>
2019-10-30 23:27:05 -0300

    v0.1 draft phase

--- index.js
+++ index.js
@@ -4,14 +4,6 @@ const Buffer = require('safe-buffer').Buffer;
 var request = require('request');
 var async = require('async');
 
-// Preparing auth header
-const API_TOKEN = "d8b71b55b1aa3acf2e07bc17d1a3759a"
-const auth = "Basic " + new Buffer(API_TOKEN + ":").toString("base64")
-const chargeEndpoint = 'https://api.iugu.com/v1/charge'
-
-// [START functions_helloworld_debug]
-require('@google-cloud/debug-agent').start();
-// [END functions_helloworld_debug]
 
 /*
 * Cloud Function.
