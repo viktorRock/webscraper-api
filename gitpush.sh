b48ea596c83c13f2c2af59fb016dae73e729f7bd
pvmathiassilva <pvmathiassilva@gmail.com>
2019-12-07 20:03:50 -0400

    v0.1 draft

--- gitpush.sh
+++ gitpush.sh
@@ -4,6 +4,6 @@ git push
 
 
 gcloud functions deploy webscraper1 \
-    --source https://source.developers.google.com/projects/webscraper-beta/repos/github_viktorrock_webscraper-beta/moveable-aliases/master/paths/+/ \
+    --source https://source.developers.google.com/projects/webscraper-beta/repos/github_viktorrock_webscraper-beta/moveable-aliases/master/paths// \
     --runtime=nodejs8 \
     --trigger-http
