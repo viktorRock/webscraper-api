84029d59ca89450ef6abf49ecc9915357798b2ba
pvmathiassilva <pvmathiassilva@gmail.com>
2019-12-07 20:07:37 -0400

    v0.1 draft

--- index.js
+++ index.js
@@ -16,24 +16,24 @@ exports.scraperGET = (req, res) => {
 };
 
 exports.webscraper1 = (req, res) => {
-	res.send("webscraper1");
-  };
-
-/* var c = new Crawler({
-    maxConnections : 10,
+  console.log("before crawl");
+  var c = new Crawler({
+    maxConnections: 10,
     // This will be called for each crawled page
-    callback : function (error, res, done) {
-        if(error){
-            console.log(error);
-        }else{
-            var $ = res.$;
-            // $ is Cheerio by default
-            //a lean implementation of core jQuery designed specifically for the server
-            console.log($("title").text());
-        }
-        done();
-    }
-});
+    callback: function(error, res, done) {
+      if (error) {
+        console.log(error);
+      } else {
+        var $ = res.$;
+        // $ is Cheerio by default
+        //a lean implementation of core jQuery designed specifically for the server
 
-// Queue just one URL, with default callback
-c.queue('http://www.amazon.com'); */
+        res.send($("title").text());
+      }
+      done();
+    }
+  });
+
+  // Queue just one URL, with default callback
+  c.queue("http://www.amazon.com");
+};
